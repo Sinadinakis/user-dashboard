5a43037af840dc5521340953aff4fe6a8bab965f
Manolis Sinadinakis <sinadinakis.e@gmail.com>
2020-10-20 17:55:55 +0200

    Fix a little mobile view

--- src/components/Table/TableRows/TableRows.js
+++ src/components/Table/TableRows/TableRows.js
@@ -11,36 +11,40 @@ import { tableHeaders } from "../TableColumns/TableColumns";
 * @param {Function} handleClick
 */
 const TableRows = ({ result, handleClick }) => {
-  return (
-    result &&
-    result?.data.map((item, index) => {
-      const { name, share_value, shareholders } = item;
-      return (
-        <div key={index} className="table__content">
-          <div className="table__rectangle"></div>
-          <div className="table__logo">
-            <span>{name.split(" ")[0]}</span>
-          </div>
-          <div className="table__info-name">{name ?? "No name"}</div>
-          <div className="table__info">
-            <div className="table__info-column">
-              <p>{tableHeaders[1]} </p>
-              <p>{tableHeaders[2]} </p>
-            </div>
-            <div className="table__info-column">
-              <p>{share_value} </p>
-              <p>{shareholders.length} </p>
-            </div>
-          </div>
-          <div className="table__button">
-            <button onClick={() => handleClick(index, item)}>
-              Manage deze onderneming
-            </button>
-          </div>
-        </div>
-      );
-    })
-  );
+    return (
+        <>
+            {result &&
+                result?.data.map((item, index) => {
+                    const { name, share_value, shareholders } = item;
+                    return (
+                        <div key={index} className="table__content">
+                            <div className="table__rectangle"></div>
+                            <div className="table__logo">
+                                <span>{name.split(" ")[0]}</span>
+                            </div>
+                            <div className="table__info-name">{name ?? "No name"}</div>
+                            <div className="table__info">
+                                <div className="table__info-column">
+                                    <p>{tableHeaders[1]} </p>
+                                    <p>{tableHeaders[2]} </p>
+                                </div>
+                                <div className="table__info-column">
+                                    <p>{share_value} </p>
+                                    <p>{shareholders.length} </p>
+                                </div>
+                            </div>
+                            <div className="table__button">
+                                <button onClick={() => handleClick(index, item)}>
+                                    Manage deze onderneming
+                                </button>
+                            </div>
+                        </div>
+                    );
+                })
+            }
+            <div className="table__content table__content--add_new">Add new</div>
+        </>
+    );
 };
 
 export default TableRows;
